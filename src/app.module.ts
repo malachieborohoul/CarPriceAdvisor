9b0f756925cd34e1b255ba0964e9e5b8986ae1b5
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-10-27 20:26:42 +0100

    Error from UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, ReportsModule]
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private repo: Repository<User>) {}
+
+  async findOne(id: number) {
+   const user= await this.repo.findOneBy({ id });
+
+   if(!user){
+    throw new NotFoundException("user not found");
+   }
+
+   return user;
+  }
+  async find(email: string) {
+   const user= await this.repo.findBy({ email });
+
+   if(!user){
+    throw new NotFoundException('user not found');
+   }
+
+   return user;
+  }
+}
