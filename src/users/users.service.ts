0df2a3e804953b5d4eed2efc7f09eb8d45d48617
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-10-27 20:41:48 +0100

    Service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,21 +8,42 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   async findOne(id: number) {
-   const user= await this.repo.findOneBy({ id });
+    const user = await this.repo.findOneBy({ id });
 
-   if(!user){
-    throw new NotFoundException("user not found");
-   }
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
 
-   return user;
+    return user;
   }
   async find(email: string) {
-   const user= await this.repo.findBy({ email });
+    const user = await this.repo.findBy({ email });
 
-   if(!user){
-    throw new NotFoundException('user not found');
-   }
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
 
-   return user;
+    return user;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+   return this.repo.remove(user);
+
   }
 }
