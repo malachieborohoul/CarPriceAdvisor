8efe061c94c94a0f02ab231dca8af3d8fd694687
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-10-29 22:19:46 +0100

    Signup and signin controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,47 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('/signup')
+  async signup(@Body() body: CreateUserDto) {
+    const user = await this.usersService.find(body.email);
+
+    if (user) {
+      throw new BadRequestException('email in use');
+    }
+
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto) {
+    const user = await this.usersService.find(body.email);
+    if (!user) {
+      throw new NotFoundException('email not found');
+    }
+
+    return this.authService.signin(body.email, body.password);
+  }
 
   @Get('/:id')
   findUser(@Param('id') id: string) {
