8db682600eb200b065274503a5033ba557dbe242
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-03 21:19:58 +0100

    Creating app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
-  imports: [
-    UsersModule,
-    ReportsModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [User],
-      synchronize: true,
-    }),
-  ],
+  providers: [AppService]
 })
 export class AppModule {}
