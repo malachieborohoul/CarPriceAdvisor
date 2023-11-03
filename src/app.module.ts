31ff8493d7d58e3f9c56e04e1848650a60de2fc7
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-03 21:22:42 +0100

    Creating reports modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule]
+  imports: [UsersModule, ReportsModule]
 })
 export class AppModule {}
