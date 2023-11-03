c12cb7658fb656843633485d66bd71224f55ad80
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-03 22:40:22 +0100

    Add signup & signin in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,14 +5,29 @@ import {
   Get,
   Param,
   Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Post('/signup')
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
 
   @Get()
   findAllUsers(@Query('email') email: string) {
