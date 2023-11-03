113282883d2426b0a44b0c0ce898cce043dfa7c2
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-03 21:45:46 +0100

    Add remove in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,4 +35,13 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.repo.remove(user);
+  }
 }
