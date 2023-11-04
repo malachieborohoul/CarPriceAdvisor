64e71ddf41d86fd533feb5bae1cbf2c960ec2c4a
BOROHOUL Soguelni Malachie <80067657+malachieborohoul@users.noreply.github.com>
2023-11-04 20:06:11 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  / app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalPipes(new ValidationPipe());
 
  app.use(cookieSession({
