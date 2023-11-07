36e9211bdb54eb3ac079befa5ddb32facb47c38f
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-07 21:54:21 +0100

    AuthService

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
     return this.usersService.create(email, result);
   }
 
-  // Signi
+  // Sign
 
   async signin(email: string, password: string) {
     const [user] = await this.usersService.find(email);
