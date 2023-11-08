85677777a5b353e8f12fa57d2de8f4a314f3817a
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-08 14:46:33 +0100

    findUser - findAllUsers - update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private usersService: UsersService){}
+  constructor(private usersService: UsersService) {}
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email:string){
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id:string, ){}
 }
