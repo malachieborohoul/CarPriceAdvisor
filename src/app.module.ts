b5a17da0381a70f7f79f464013c2ff0817cf350b
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-08 14:11:52 +0100

    users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
   providers: [AppService],
-  controllers: [AppController]
+  controllers: [AppController],
+  imports: [UsersModule]
 })
 export class AppModule {}
