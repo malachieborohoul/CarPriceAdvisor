bac1611944b0aa86ea4383b179723b20d06c9690
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-08 22:58:54 +0100

    SerializeInterceptor

--- src/users/interceptors/serialize.interceptor.ts
+++ src/users/interceptors/serialize.interceptor.ts
@@ -10,7 +10,7 @@ import { Observable, map } from 'rxjs';
 export const Serialize = (dto: any) => {
   return UseInterceptors(new SerializeInterceptor(dto));
 };
-// SerializeInte
+// SerializeInt
 export class SerializeInterceptor implements NestInterceptor {
   constructor(private dto: any) {}
   intercept(
