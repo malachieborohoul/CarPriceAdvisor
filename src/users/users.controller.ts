0829f9d321ca57ef849a20fa501765ae256ab510
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-09 20:04:14 +0100

    CurrentUserDecorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -32,7 +32,7 @@ export class UsersController {
   whoami(@CurrentUser() user: User) {
     return user;
   }
-// SIGNU
+// SIGN
   @Post('/signup')
   async signup(@Body() body: CreateUserDto, @Session() session:any) {
     const user = await this.usersService.find(body.email);
