257951416f6466ed92de22dc75be5c285bfed122
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-10 20:07:23 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,8 +2,14 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 
-it('create an auth service instance',async ()=>{
-  const fakeUsersService ={
-    find: ()=>Promise.resolve([])
-  }
-})
+it('create an auth service instance', async () => {
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password }),
+  };
+
+  const module = Test.createTestingModule({
+    providers:[AuthService]
+  })
+});
