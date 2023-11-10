19b8b378c6351bfba772e47b487e8eb54707b4dd
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-10 20:32:16 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,27 +1,30 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
-let service: AuthService;
-beforeEach(async () => {
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
+describe('AuthService', () => {
+  let service: AuthService;
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
 
-  service = module.get(AuthService);
-});
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        }, 
+      ],
+    }).compile();
 
-it('create an auth service instance', async () => {
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('create an auth service instance', async () => {
+    expect(service).toBeDefined();
+  });
 });
