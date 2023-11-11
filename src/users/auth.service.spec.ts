44903a9a04b437c2f769c962c78b4cd8a100490e
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-11 20:30:07 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,5 +1,12 @@
-describe('Auth Service', ()=>{
-  beforeEach(()=>{
+import { UsersService } from './users.service';
 
-  })
-})
+describe('Auth Service', () => {
+  let fakeUsersService: Partial<UsersService>;
+  beforeEach(() => {
+    fakeUsersService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+  });
+});
