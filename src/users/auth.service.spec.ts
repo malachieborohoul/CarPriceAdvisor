5e3f9e50958a6b0a594f81e4c09ba0ec0b3396be
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-11 20:49:05 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -5,14 +5,14 @@ import { AuthService } from './auth.service';
 describe('Auth Service', () => {
   let fakeUsersService: Partial<UsersService>;
   let service: AuthService;
-  beforeEach(async() => {
+  beforeEach(async () => {
     fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password }),
     };
 
-    const module =  await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       providers: [
         AuthService,
         {
@@ -25,18 +25,18 @@ describe('Auth Service', () => {
     service = module.get(AuthService);
   });
 
-  it('create an authservice instance', ()=>{
-    expect(service).toBeDefined()
-  })
+  it('create an authservice instance', () => {
+    expect(service).toBeDefined();
+  });
 
-  it('create a user with salted and hashed password', async()=>{
-    const user = await service.signup("mal@gmail.com", "12345");
+  it('create a user with salted and hashed password', async () => {
+    const user = await service.signup('mal@gmail.com', '12345');
 
-    expect(user.password).not.toEqual("12345");
+    expect(user.password).not.toEqual('12345');
 
-    const [salt, hash]= user.password.split('.');
+    const [salt, hash] = user.password.split('.');
 
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
-  })
+  });
 });
