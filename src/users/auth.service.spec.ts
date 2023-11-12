c11e101504df295bf3593dba90461ba626c23172
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-12 23:03:51 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -70,7 +70,7 @@ describe('Auth Service', () => {
   });
 
   
-// return
+// retur
   it('returns a user if password correct', async () => {
     //   fakeUsersService.find = () =>
     //     Promise.resolve([{ id: 1, email: 'a', password: 'a' }]);
