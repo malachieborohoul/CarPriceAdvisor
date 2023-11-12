edeb55aec84174599ad253a420d962378bd720a3
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-12 17:37:56 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -31,7 +31,7 @@ describe('Auth Service', () => {
 
   it('creates a new user with a salted and hashed password', async () => {
     const user = await service.signup('bsm@gmail.com', '12345');
-    const [salt, hash]= user.password.split('&')
+    const [salt, hash]= user.password.split('.')
     expect(user.password).not.toEqual("12345")
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
