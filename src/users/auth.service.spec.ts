f943db821dc24569ca9818b93f5e15b199f31551
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-12 13:58:55 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -29,38 +29,5 @@ describe('Auth Service', () => {
     expect(service).toBeDefined();
   });
 
-  it('create a user with salted and hashed password', async () => {
-    const user = await service.signup('mal@gmail.com', '12345');
-
-    expect(user.password).not.toEqual('12345');
-
-    const [salt, hash] = user.password.split('.');
-
-    expect(salt).toBeDefined();
-    expect(hash).toBeDefined();
-  });
-  
-  it('throws an error if users signs up with email in use', async (done) => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'bsm@gmail.com', password: '12345' }]);
-    try {
-      const user = await service.signup('bsm@gmail.com', '12345');
-      expect(user).toBe({});
-    } catch (err) {
-      done();
-    }
-  });
-
-  it('throws if signin is called with an unused email', (done) => {
-    service.signin('bsl@gmail.com', '12345').catch(() => {
-      done();
-    });
-  });
-  it('throws if an invalid password is provided', async (done) => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'ddkdk@d.com', password: 'djdjd' }]);
-    service.signin('dddjj@ddj.com', 'ZZZZ');
-    try {
-    } catch (error) {}
-  });
+ 
 });
