16a998de0e00cc462e9883cd338fad12e6fca70b
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-13 23:59:13 +0100

    AuthService Test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,4 +1,6 @@
+import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>;
@@ -9,5 +11,15 @@ describe('AuthService', () => {
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password }),
     };
+
+    const module = Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    });
   });
 });
