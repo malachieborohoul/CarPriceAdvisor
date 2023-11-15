f76d49df695caee2e81e8e65aeac114296397b27
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-15 23:08:48 +0100

    Test on AuthService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,11 +2,12 @@ import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 
+// AuthService Test
 describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>;
   let service: AuthService;
 
-  beforeEach(async() => {
+  beforeEach(async () => {
     fakeUsersService: {
       find: () => Promise.resolve([]);
       create: (email: string, password: string) =>
@@ -26,6 +27,17 @@ describe('AuthService', () => {
     service = module.get(AuthService);
   });
   it('can create an authservice instance', async () => {
-    expect(service).toBeDefined()
+    expect(service).toBeDefined();
+  });
+  it('creates a new user with a salted and hashed password', async() => {
+    fakeUsersService.find = ()=>Promise.resolve([{id:1,email:'a', password:'a'}])
+    const [user] = await fakeUsersService.find('m')
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+
+    expect('a').not.toEqual(hash)
   });
 });
