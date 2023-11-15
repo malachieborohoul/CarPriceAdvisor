e4fe2df23f8d35e2e4689bbc429669374e185033
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-15 21:48:06 +0100

    Test on AuthService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,45 +4,15 @@ import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>;
-  let service: AuthService;
 
-  beforeEach(async () => {
-    fakeUsersService = {
-      find: () => Promise.resolve([]),
+  beforeEach(() => {
+    fakeUsersService: {
+      find: () => Promise.resolve([]);
       create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password }),
-    };
+        Promise.resolve({ id: 1, email, password });
+    }
 
-    const module = await Test.createTestingModule({
-      providers: [
-        AuthService,
-        {
-          provide: UsersService,
-          useValue: fakeUsersService,
-        },
-      ],
-    }).compile();
-
-    service = module.get(AuthService);
+    const module = Test
   });
-
-  it('can create an authservice instance', () => {
-    expect(service).toBeDefined();
-  });
-
-  it('create a new user with a salted and hashed user', async () => {
-    const user = await service.signup('bsm', '12345');
-
-    const [salt, hash] = user.password.split('.');
-    expect('12345').not.toEqual(user.password);
-    expect(salt).toBeDefined();
-    expect(hash).toBeDefined();
-  });
-
-  it('throws error if email is in use',(done)=>{
-    fakeUsersService.find=()=>Promise.resolve([{id:1, email:"a", password:"a"}]);
-    service.signup('b','b').catch(()=>{
-      done()
-    }) 
-  })
+  it('can create an authservice instance', async () => {});
 });
