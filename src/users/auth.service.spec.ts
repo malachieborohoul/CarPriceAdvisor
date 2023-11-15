0f3fb91ad694ac0ba85636ba972e4eeb7cdcd64f
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-15 23:59:10 +0100

    Test on AuthService

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -49,20 +49,26 @@ describe('AuthService', () => {
   });
 
   it('throws error if signin is called with an unused email', (done) => {
-    
-    service.signin('a', 'a').catch(()=>{
-      done()
-    })
+    service.signin('a', 'a').catch(() => {
+      done();
+    });
   });
 
-  it('throws if invalid password is provided',(done)=>{
+  it('throws if invalid password is provided', (done) => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: 'a' }]);
+    service.signin('a', 'a').catch(() => {
+      done();
+    });
+  }); 
+
+  it('returns a user if password correct', async()=>{
     fakeUsersService.find = () =>
     Promise.resolve([{ id: 1, email: 'a', password: 'a' }]);
-    service.signin('a', 'a').catch(()=>{
-      done();
-    })
+
+    const user = await service.signin('bsm4@gmail.com','123456'); 
 
     
-
+    
   })
 });
