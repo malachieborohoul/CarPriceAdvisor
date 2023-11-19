30a1a55ef285975073a9de894b9adff7d053fd00
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-19 21:27:25 +0100

    AuthService Testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,22 +3,27 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 let fakeUsersService: Partial<UsersService>;
 let service: AuthService;
-describe('AuthService', async() => {
-   fakeUsersService = {
+describe('AuthService', () => {
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+  fakeUsersService = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
       Promise.resolve({ id: 1, email, password }),
   };
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
-      },
-    ],
-  }).compile();
-
-  service = module.get(AuthService);
+  it('can create an authservice instance', () => {
+    expect(service).toBeDefined();
+  });
 });
