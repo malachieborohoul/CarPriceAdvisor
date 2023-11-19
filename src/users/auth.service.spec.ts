b21abf9766c3dfb3aa37c31d231bc8c22a59517f
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-19 21:38:51 +0100

    AuthService Testing

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,7 +4,7 @@ import { UsersService } from './users.service';
 let fakeUsersService: Partial<UsersService>;
 let service: AuthService;
 
-// AuthService
+// AuthServic
 describe('AuthService', () => {
   beforeEach(async () => {
     const module = await Test.createTestingModule({
