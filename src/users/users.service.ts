ae22d4ec077c090c2b60c1113f93fa0e276ef3ee
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 21:02:45 +0100

    User entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(){}
+    constructor(@InjectRepository(User) userRepository: Repository<User>){
+        
+    }
 }
