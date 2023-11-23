29900af83f945a133c2b253e2abb72cd13b577f2
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 21:46:33 +0100

    DTOs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(usersService: UsersService){}
+
+    update(@Param('id') id:string, @Body() body: CreateUserDto){}
+}
