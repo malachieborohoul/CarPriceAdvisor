8a06d2d7ecb32265b63f00f55459fbbdf2aa419e
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 00:27:57 +0100

    Interceptors

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,7 +1,13 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
 import { Observable, map } from "rxjs";
+interface ClassContainer{
+    new (...args:any[]):{}
+}
 
+export const Serialize = (dto:ClassContainer)=>{
+     return UseInterceptors(new SerializeInterceptor(dto))
+}
 export class SerializeInterceptor implements NestInterceptor{
     constructor(private dto: any){}
     intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
