546c0602c749a95f636edf4ec8aa760e37bdcd1d
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 23:19:25 +0100

    UsersService

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt)
+
+// Injectabl
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService){}
+    async signup(email:string, password:string){
+       const user = await this.usersService.find(email)
+
+       if(user.length){
+        throw new BadRequestException('email in use');
+       }
+
+       const salt = randomBytes(24).toString()
+
+
+    }
+}
