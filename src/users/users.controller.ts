56cca3b9ce9eecde0a4549b2d110fe2defa9fd84
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-24 00:01:25 +0100

    Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,48 @@
-import { Body, Controller, Delete, Get, Param, Patch, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
-  @Get('id')
-  findUser(@Param('id') id: string){
-    return this.usersService.findOne(parseInt(id))
+  @Post()
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(body.email, body.password);
+  }
+
+  @Post()
+  signin(@Body() body: CreateUserDto){
+    return this.authService.signin(body.email, body.password);
+  }
+  @Get('/id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
   }
   @Get()
-  findAllUsers(@Query('email') email:string){
+  findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
   }
 
-  @Patch('id')
+  @Patch('/id')
   update(@Param('id') id: string, @Body() body: CreateUserDto) {
     return this.usersService.update(parseInt(id), body);
   }
 
-  @Delete('id')
+  @Delete('/id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
