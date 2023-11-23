d16e0e4a056ecc49c3fe06c59d045e12baec90c5
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 22:20:19 +0100

    UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
@@ -6,16 +6,21 @@ import { CreateUserDto } from './dtos/create-user.dto';
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Get('id')
   findUser(@Param('id') id: string){
     return this.usersService.findOne(parseInt(id))
   }
+  @Get()
   findAllUsers(@Query('email') email:string){
     return this.usersService.find(email);
   }
+
+  @Patch('id')
   update(@Param('id') id: string, @Body() body: CreateUserDto) {
     return this.usersService.update(parseInt(id), body);
   }
 
+  @Delete('id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
