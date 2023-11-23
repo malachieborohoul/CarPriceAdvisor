8c5669391cee95092401005dcacd20df499337ef
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-23 20:50:23 +0100

    User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule]
+  imports: [UsersModule,
+    TypeOrmModule.forRoot({
+      type:"mysql",
+      database: "db",
+      entities:[User]
+    })
+  ]
 })
 export class AppModule {}
