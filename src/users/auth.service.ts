34c646a8317a6f278341f87bdbe23d3bef8ec035
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-26 22:46:09 +0100

    UsersCOntrollers

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -9,7 +9,7 @@ import { promisify } from 'util';
 
 const scrypt = promisify(_scrypt);
 
-//InjectableInjec
+//InjectableInje
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
