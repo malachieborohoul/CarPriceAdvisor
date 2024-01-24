b28e6a46d4ae38f83c0a11ec4a3f1ded3fe58c09
BOROHOUL Soguelni Malachie <80067657+malachieborohoul@users.noreply.github.com>
2024-01-24 23:08:46 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   //   }),
   // );
 //
-//ghhgh
+//ghhghffstageghhghffstageghhghffstageghhghffstag
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
